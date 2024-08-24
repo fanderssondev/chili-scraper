27fade65bee1505ca4943522e124d0a51adc626e
Fredrik <fredrikanderssondev@gmail.com>
2024-08-24 19:59:04 +0200

    overview minus pics fetched

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
+import { title } from 'process';
 import puppeteer from 'puppeteer';
+
+interface Product {
+  sku: string;
+  title: string;
+  hotness: number;
+  price: number;
+  pictures: {
+    smallPic: string;
+    largePics: string[];
+  };
+  shortDescription: string;
+  description: string;
+}
 
 const baseURL = 'https://www.pepperworldhotshop.com/en/bbq-shop/fresh-chillies/';
 
@@ -9,11 +23,25 @@ const scrape = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto(baseURL, { waitUntil: 'domcontentloaded' });
+  console.log('before');
+  await page.goto(baseURL, { waitUntil: 'networkidle0' });
+  console.log('after');
 
-  const res = await page.$$eval('.productList > .productBox.product-box', (elements) => {
+  const res = await page.$$eval('.productList > .productBox.product-box > .borderbox > form', (elements) => {
     return elements.map((e) => {
-      return e.classList;
+      return {
+        title: e.querySelector<HTMLDivElement>('.listDetails > .title')?.innerText ?? 'FAILED',
+        hotness: +(e.querySelector<HTMLImageElement>('.listDetails > .hotness > img')?.title.split(' ')[1] ?? 0),
+        price: +(
+          e
+            .querySelector<HTMLAnchorElement>('.listDetails > .price > .content > .lead > a')
+            ?.innerText.split(' ')[0]
+            .replace(',', '.') ?? 0
+        ),
+        pictures: {
+          smallPic: e.querySelector<HTMLImageElement>('img')?.src,
+        },
+      };
     });
   });
 
@@ -25,4 +53,5 @@ const scrape = async () => {
 
 scrape();
 
-// productList row productSlider grid-view newItems
+// https://www.pepperworldhotshop.com/out/pictures/generated/product/1/160_155_75/ba097_die-letzten-ihrer-art.jpg
+// https://www.pepperworldhotshop.com/out/pictures/master/product/1/ba097_die-letzten-ihrer-art.jpg
