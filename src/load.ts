f1664b33501b37c7d3f2b920636561d49e391ac1
Fredrik <fredrikanderssondev@gmail.com>
2024-10-03 17:51:14 +0200

    refactored loading of products and seeding products

--- src/load.ts
+++ src/load.ts
@@ -1,35 +1,9 @@
 import { db } from './db';
 import * as fs from 'fs';
 
-interface Product {
-  id: string;
-  title: string;
-  sku: string;
-  slug: string;
-  price: number;
-  pictures: {
-    smallPic: string;
-    largePics: string[];
-  };
-  description: {
-    description_short: string;
-    description_long: string;
-  };
-  product_details: {
-    category: string;
-    manufacturer: string;
-    hotness: number;
-    weight: number;
-    rating: {
-      average: number;
-      nr_of_reviews: number;
-    };
-  };
-  url: string;
-}
 
 const productsLoadFromDB = async () => {
-  const products = await db.product.findMany({
+  return await db.product.findMany({
     select: {
       title: true,
       sku: true,
@@ -60,39 +34,8 @@ const productsLoadFromDB = async () => {
   });
 };
 
-const seed;
-
 const main = async () => {
-
-
-
-
-  // fs.writeFile('products.json', JSON.stringify(products, null, 2), (err) => {});
-  // console.log(JSON.stringify(products, null, 3));
-
-  // const products: Product[] = JSON.parse(fs.readFileSync('./products2.json', { encoding: 'utf-8' }));
-
-  // for (const product of products) {
-  //   let newPics: string[] = [];
-
-  //   for (const pics of product.pictures) {
-  //     const renamed = pics.replace('.jpg', '.png');
-  //     newPics.push(renamed);
-  //   }
-
-  //   // console.log(newPics);
-
-  //   await db.product.update({
-  //     where: {
-  //       id: product.id,
-  //     },
-  //     data: {
-  //       pictures: newPics,
-  //     },
-  //   });
-  // }
-
-  // console.log(JSON.stringify(products, null, 3));
+  // await productsLoadFromDB();
 };
 
 main()
